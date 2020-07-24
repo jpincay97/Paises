0eb9944be3026f6f78706355bb5db8f3e1dfb70e
John Pincay <jpincay97@users.noreply.github.com>
2020-07-23 19:37:39 -0500

    Commit

---
+++ app/src/main/java/com/example/paises/Model/Pais.java
@@ -0,0 +1,19 @@
+package com.example.paises.Model;
+
+public class Pais {
+
+    private String name;
+
+    public Pais(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
